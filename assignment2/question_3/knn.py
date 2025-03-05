1b3ceb0d90c9cc95f3b5fab0b016c87042195845
Soap504 <149292744+Soap504@users.noreply.github.com>
2025-03-04 20:05:36 -0800

    Final Renditions

--- assignment2/question_3/knn.py
+++ assignment2/question_3/knn.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
+# AUTHOR: Sophia Arce
 # FILENAME: knn.py
 # SPECIFICATION: Compute the LOO-CV error rate for a 1NN classifier on spam/ham classification
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 7 hours
 # -----------------------------------------------------------*/
 
 # IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
